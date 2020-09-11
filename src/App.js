4700b63bcd1d1ced12c9306ad07b923628afe417
Haritha Gonuguntla <Haritha@Harithas-MacBook-Pro.local>
2020-09-11 09:57:41 -0500

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: 'React'
+  }
+
+  changeName = (newName) => {
+    this.setState({
+      name: newName
+    })
+  }
+
+  changeNameFromInput = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <button onClick={() => this.changeName('Awesome React :(')}>Change state using Anon Function</button>
+        <button onClick={this.changeName.bind(this, 'Awesome React :)')}>Change state using bind Function</button>
+        <br /> <br />
+        <input type="text" value={this.state.name} onChange = {this.changeNameFromInput}/>
+        <br /> <br />
+        <div>{this.state.name}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
